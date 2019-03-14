00e82caa10c58f8f7b7b5052bb1f4927fc13fcc5
Victor Hermes <me.victorhermes@gmail.com>
2019-03-13 21:04:38 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import TodoList from './TodoList';
+
+const App = () => (
+  // O provider vai passar para todos os componentes dentro dele as informações do redux
+
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+);
 
 export default App;
