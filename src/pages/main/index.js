f32332f9682b034210ad1c19744fd7c9af61cb71
Victor Hermes <me.victorhermes@gmail.com>
2019-03-14 21:07:22 -0300

    Configuração redux

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,13 +1,39 @@
 import React, { Fragment, Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default class Main extends Component {
+// connect do react-redux tem o papel de conectar o componente com algum reducer
+import { connect } from 'react-redux';
+
+// o bindActionCreators vai aplicar a cada action, um dispatch
+import { bindActionCreators } from 'redux';
+import * as FavoriteActions from '../../store/actions/favorites';
+
+class Main extends Component {
+  static propTypes = {
+    addFavorite: PropTypes.func.isRequired,
+    favorites: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        url: PropTypes.string,
+      }),
+    ).isRequired,
+  };
+
   state = {
     repositoryInput: '',
   };
 
+  handleAddRepository = (event) => {
+    const { addFavorite } = this.props;
+    event.preventDefault();
+    addFavorite();
+  };
+
   render() {
     const { repositoryInput } = this.state;
-
+    const { favorites } = this.props;
     return (
       <Fragment>
         <form onSubmit={this.handleAddRepository}>
@@ -20,16 +46,42 @@ export default class Main extends Component {
         </form>
 
         <ul>
-          <li>
-            <p>
-              <strong> facebook/react </strong>
-              {' '}
-(A declarative spotlight that have never seen)
-            </p>
-            <a href="http://github.com/facebook/react"> Acessar </a>
-          </li>
+          {favorites.map(favorite => (
+            <li key={favorite.id}>
+              <p>
+                <strong>
+                  {' '}
+                  {favorite.name}
+                  {' '}
+                </strong>
+                {' '}
+(
+                {favorite.description}
+)
+              </p>
+              <a href={favorite.url}> Acessar </a>
+            </li>
+          ))}
         </ul>
       </Fragment>
     );
   }
 }
+
+// mapStateToProps transformará o state do reducer em uma prop para consumir no componente
+// o state do mapStateToProps representa todas as informações contidas no reducer
+// o state.favorites é o reducer de function favorites
+
+const mapStateToProps = state => ({
+  favorites: state.favorites,
+});
+
+// transforma as actions em props
+// tudo o que tem no => dispatch é o que os reducers irão ouvir
+// o bindActionCreators vai aplicar um dispatch a cada action. O FavoriteActions são as actions
+const mapDispatchToProps = dispatch => bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Main);
---
+++ src/store/reducers/favorites.js
@@ -0,0 +1,25 @@
+// Reducer sempre irá ouvir uma action disparada
+// É através da action que conseguimos fazer alguma coisa
+// (state, action) => o state é o estado atual, sem modificação
+// o type é como se fosse o id da action
+// a action são as informações disparadas pelo usuário
+// o default é útil porque caso uma action não precise fazer nada, o reducer retorna default
+
+const INITIAL_STATE = [];
+
+export default function favorites(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'ADD_FAVORITE':
+      return [
+        ...state,
+        {
+          id: Math.random(),
+          name: 'facebook/react',
+          description: 'Oh yeah',
+          url: 'https://github.com/facebook/react',
+        },
+      ];
+    default:
+      return state;
+  }
+}
