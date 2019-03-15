d50f5154ada54d21c7718192aacead0cf0e02d10
Victor Hermes <me.victorhermes@gmail.com>
2019-03-14 22:36:13 -0300

    Components

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from '../pages/main';
+import Footer from '../components/Footer';
 
 const Routes = () => (
   // Como o react router dom tem integração com o native,...
   // ...o BrowserRouter é para dizer que a página será rodada na internet
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Main} />
-    </Switch>
+    <Fragment>
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+
+      <Footer />
+    </Fragment>
   </BrowserRouter>
 );
 
