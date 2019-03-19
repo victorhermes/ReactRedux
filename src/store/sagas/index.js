0930a53245f05054b2d811cb6dcf0af2405db6d2
Victor Hermes <me.victorhermes@gmail.com>
2019-03-18 23:32:13 -0300

    Sagas

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,6 +1,6 @@
 // O redux-saga é um middleware que vai entre a action e o reducer
-// Action, saga e reducer
-// É no sagas que fazer requisições a api
+// Action -> saga -> reducer
+// É no sagas que faz requisições a api
 // Dentro do array all ficarão os listeners, que são funções do saga para ouvir as actions
 // Além disso o array envia algumas informações para os reducers
 // o All é como se fosse o combineReducers, só que pra sagas
